d1e62a7a4095e5f54257d0d6ffedfa7a048b6507
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-08-26 04:08:49 +0100

    Random lvl gen in 20min lol
    - To change the size of enemy generation go to MissionData in Resources folder
    - The names of the enemies to be genetized are in the GameManager object

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public MissionData missionData;
+
+    public string[] enemyNames; // List of possible enemy names
+
+    void Start()
+    {
+        Mission selectedMission = missionData.missions[Random.Range(0, missionData.missions.Length)];
+
+        foreach (EnemyInfo enemyInfo in selectedMission.enemyInfos)
+        {
+            // enemyInfo.id = /* Set id */;
+            enemyInfo._name = enemyNames[Random.Range(0, enemyNames.Length)];
+            enemyInfo.level = 0;
+            // enemyInfo.obstacles = Obstacles.None;
+        }
+    }
+}
